6f6b7ad6c4cb857df8645150e50a5d87064f215d
Frederico Pellegrini <fredericopellegrini@proton.me>
2024-10-21 12:10:10 -0300

    Add comparision logic

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 console.log("Hello World!")
 
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice(){
     let computerChoice = Math.random();
 
         if (computerChoice < 1/3){
-            computerChoice = "Rock"
+            computerChoice = "rock"
         } else if (computerChoice < 2/3){
-            computerChoice = "Paper"
+            computerChoice = "paper"
         } else {
-            computerChoice = "Scissors"
+            computerChoice = "scissors"
         }
 
     console.log(computerChoice);
+    return computerChoice;
 }
 
+function getHumanChoice(){
+    let humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+    console.log(humanChoice)
+    return humanChoice;
+}
 
-getComputerChoice();
+function playRound(humanChoice, computerChoice){
+    if (humanChoice == computerChoice) {
+        console.log("Empate!");
+    } else if (
+        (humanChoice == "rock" && computerChoice == "scissors") ||
+        (humanChoice == "scissors" && computerChoice == "paper") ||
+        (humanChoice == "paper" && computerChoice == "rock")
+    ) {
+        console.log("Você ganhou!");
+        humanScore++;
+    } else {
+        console.log("Você perdeu!")
+        computerScore++;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
