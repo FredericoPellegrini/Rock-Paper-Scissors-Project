d8bada528f7209271374edb178286f01d412acb3
Frederico Pellegrini <fredericopellegrini@proton.me>
2024-10-22 09:38:01 -0300

    Full complete

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-console.log("Hello World!")
+let round = 0;
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playGame (){
+
+
 
 
 function getComputerChoice(){
@@ -15,33 +19,51 @@ function getComputerChoice(){
             computerChoice = "scissors"
         }
 
-    console.log(computerChoice);
+    console.log(`Computer: ${computerChoice}`);
     return computerChoice;
 }
 
 function getHumanChoice(){
     let humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
-    console.log(humanChoice)
+    console.log(`Human: ${humanChoice}`)
     return humanChoice;
 }
 
 function playRound(humanChoice, computerChoice){
     if (humanChoice == computerChoice) {
-        console.log("Empate!");
+        console.log("Draw!");
     } else if (
         (humanChoice == "rock" && computerChoice == "scissors") ||
         (humanChoice == "scissors" && computerChoice == "paper") ||
         (humanChoice == "paper" && computerChoice == "rock")
     ) {
-        console.log("Você ganhou!");
+        console.log("You win!");
         humanScore++;
     } else {
-        console.log("Você perdeu!")
+        console.log("You loose!")
         computerScore++;
     }
 }
+
+console.log(`Round: ${round + 1}`)
+
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection)
+
+}
+
+
+while (round < 5){
+    playGame();
+    round++;
+}
+
+console.log("-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+console.log("Final Score");
+console.log(`Human: ${humanScore}`);
+console.log(`Computer: ${computerScore}`)
+console.log("-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
